67554a24ee520646dd58e20f91e90cc77de3c34e
Jade Tabony <jade@metabrite.com>
2018-03-08 12:34:12 -0800

    More refactor

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,21 +8,19 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             multiplier = 11
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    if item.quality > 0:
-                        item.quality -= 1
+            if self.decreases_quality_over_time(item):
+                self._adjust_quality(item, -1)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    self._adjust_quality(item, 1)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = item.quality + 1
+                        self._adjust_quality(item, 1)
                         if item.quality < 50:
                             if item.sell_in < 11:
                                 item.quality = item.quality + 1
                             if item.sell_in < 6:
                                 item.quality = item.quality + 1
-                        item.quality = item.quality - 1
+                        self._adjust_quality(item, -1)
             if "Sulfuras, Hand of Ragnaros" != item.name:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -33,14 +31,20 @@ class GildedRose(object):
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        self._adjust_quality(item, -item.quality)
                 else:
                     multiplier = 0
                     if item.quality < 50:
-                        item.quality += 1
+                        self._adjust_quality(item, 1)
                     else:
                         pass
 
+    def _adjust_quality(self, item, delta):
+        if item.quality > 0:
+            item.quality += delta
+
+    def decreases_quality_over_time(self, item):
+        return item.name not in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros")
 
 class Item:
     def __init__(self, name, sell_in, quality):
